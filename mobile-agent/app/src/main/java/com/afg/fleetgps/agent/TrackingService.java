8550b31b730f945ece007107a93971b1f1a18db2
wali1264 <walinimroozi@gmail.com>
2026-09-07 09:44:18 -0700

    update

--- mobile-agent/app/src/main/java/com/afg/fleetgps/agent/TrackingService.java
+++ mobile-agent/app/src/main/java/com/afg/fleetgps/agent/TrackingService.java
@@ -10,6 +10,7 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
